9045c0b7539bb43a0dca97a7449e4f5fd36e1b22
emcconkey <emcconkey@stillsolutions.com>
2014-09-14 14:50:09 -0600

    More path fixes

--- init.cmd
+++ init.cmd
@@ -5,11 +5,13 @@ REM -
 REM - This script will download and execute the latest version
 REM - of cleartmp from github
 REM - ***********************************************************
-
+setlocal enableextensions
+%~d0
+cd %~p0
 echo Getting the latest version of cleartmp
 wget -O master.zip --no-check-certificate "https://github.com/emcconkey/cleartmp/archive/master.zip"
 7za x master.zip
-copy cleartmp-master/* .
+copy cleartmp-master\* .
 rmdir /s /q cleartmp-master
 del master.zip
 echo Running cleartmp
